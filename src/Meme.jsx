c4761dcc7743ae15cb9260a6157c53bcad34bb80
Olga <minaeva9@gmail.com>
2024-03-30 18:34:23 -0400

    form styling

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,3 +1,13 @@
+import memesData from "./memesData";
+
+function getMemeImage() {
+  const memesArray = memesData.data.memes;
+  const random = Math.floor(Math.random() * memesArray.length);
+  const url = memesArray[random].url;
+  console.log(url);
+}
+getMemeImage();
+
 export default function Meme() {
   return (
     <main>
@@ -10,7 +20,9 @@ export default function Meme() {
           Bottom Text
           <input id="bottom-text" type="text" className="form--input" />
         </label>
-        <button className="form--button">Get a new meme image 🖼</button>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
       </form>
     </main>
   );
